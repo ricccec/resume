6c59a45a5389c2ecfca7950fbc772135aef47b91
ricccec <65662894+ricccec@users.noreply.github.com>
2026-03-16 12:28:24 +0100

    fix(resume): Use paragraph column for all subheadings to prevent overflow

--- scripts/json2latex.js
+++ scripts/json2latex.js
@@ -108,7 +108,7 @@ const tpl = `%-------------------------
 
 \\newcommand{\\resumeSubheading}[4]{
   \\vspace{-2pt}\\item
-    \\begin{tabular*}{0.97\\textwidth}[t]{l@{\\extracolsep{\\fill}}r}
+    \\begin{tabular*}{0.97\\textwidth}[t]{p{0.75\\textwidth}@{\\extracolsep{\\fill}}r}
       \\textbf{#1} & #2 \\\\
       \\textit{\\small#3} & \\textit{\\small #4} \\\\
     \\end{tabular*}\\vspace{-7pt}
@@ -116,14 +116,14 @@ const tpl = `%-------------------------
 
 \\newcommand{\\resumeSubSubheading}[2]{
     \\item
-    \\begin{tabular*}{0.97\\textwidth}{l@{\\extracolsep{\\fill}}r}
+    \\begin{tabular*}{0.97\\textwidth}{p{0.75\\textwidth}@{\\extracolsep{\\fill}}r}
       \\textit{\\small#1} & \\textit{\\small #2} \\\\
     \\end{tabular*}\\vspace{-7pt}
 }
 
 \\newcommand{\\resumeProjectHeading}[2]{
     \\item
-    \\begin{tabular*}{0.97\\textwidth}{l@{\\extracolsep{\\fill}}r}
+    \\begin{tabular*}{0.97\\textwidth}{p{0.75\\textwidth}@{\\extracolsep{\\fill}}r}
       \\small#1 & #2 \\\\
     \\end{tabular*}\\vspace{-7pt}
 }
